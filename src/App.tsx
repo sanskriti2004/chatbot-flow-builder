140cf2b805625d48b830c0fd91ba503db1a7636f
sanskriti2004 <sanskritigogoi2004@gmail.com>
2026-02-26 17:33:39 +0530

    feat: setup main application and routing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { FlowBuilder } from "./components/FlowBuilder";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <FlowBuilder />
+    </div>
+  );
+}
+
+export default App;
